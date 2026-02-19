a3a64365677d6abb3030595e619e4bdda21357fe
Semyon Kononchuk <semyon.kononchuk@loewensteinmedical.com>
2026-02-19 12:57:09 +0100

    Try to fix ingress configs

--- stirling_pdf/run.sh
+++ stirling_pdf/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-exec java -Dfile.encoding=UTF-8 -jar /app.jar
 
+# Configure for Home Assistant ingress
+export SYSTEM_ROOTURIPATH=/
+export SECURITY_CSRFDISABLED=false
+export CORS_ALLOWEDORIGINS=https://home.kononchuk.me
+
+exec java -Dfile.encoding=UTF-8 -jar /app.jar
