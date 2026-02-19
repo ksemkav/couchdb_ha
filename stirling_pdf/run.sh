2967d5afaefe632c1941d585b22f72d22e40e8d8
Semyon Kononchuk <semyon.kononchuk@loewensteinmedical.com>
2026-02-19 13:13:22 +0100

    Try java params

--- stirling_pdf/run.sh
+++ stirling_pdf/run.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # Get the ingress path from Home Assistant
-if [ -n "$INGRESS_ENTRY" ]; then
-    export SYSTEM_ROOTURIPATH="$INGRESS_ENTRY"
-else
-    export SYSTEM_ROOTURIPATH=/
-fi
+CONTEXT_PATH="${INGRESS_ENTRY:-/}"
 
-# Configure for Home Assistant ingress
-#export SECURITY_CSRFDISABLED=false
-export CORS_ALLOWEDORIGINS=https://home.kononchuk.me
-
-exec java -Dfile.encoding=UTF-8 -jar /app.jar
+# Start Stirling PDF with proper Spring Boot configuration for ingress
+exec java -Dfile.encoding=UTF-8 \
+    -Dserver.servlet.context-path="$CONTEXT_PATH" \
+    -Dserver.forward-headers-strategy=framework \
+    -Dserver.use-forward-headers=true \
+    -jar /app.jar
