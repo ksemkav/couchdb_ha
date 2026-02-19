72dd0d99e74dfa66b65a7669c46a24cb8f85d0c1
Semyon Kononchuk <semyon.kononchuk@loewensteinmedical.com>
2026-02-19 13:27:48 +0100

    Remove ingress for Stirling PDF

--- stirling_pdf/run.sh
+++ stirling_pdf/run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# Configure for Home Assistant ingress using Stirling PDF environment variables
-export SYSTEM_ROOTURIPATH="${INGRESS_ENTRY:-/}"
-
 exec java -Dfile.encoding=UTF-8 -jar /app.jar
